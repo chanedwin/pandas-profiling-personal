6837c863b0ccc819aa141aebef9e047c5573e30b
Edwin Chan <edwinchan@u.nus.edu>
2020-10-03 16:45:39 +0800

    add .travis.yml with install rules

--- install-spark.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl http://apache.mirror.gtcomm.net/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz --output /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
-cd /tmp && tar -xvzf /tmp/spark-$SPARK_VERSION-bin-hadoop$HADOOP_VERSION.tgz
